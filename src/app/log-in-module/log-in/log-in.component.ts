139850526c4264c237602c2deeaf781ba1189393
Radu-Branzei <115561060+Radu-Branzei@users.noreply.github.com>
2023-06-29 02:41:23 +0300

    Changed log in button to send payload to service.

--- src/app/log-in-module/log-in/log-in.component.ts
+++ src/app/log-in-module/log-in/log-in.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CustomValidators } from '../helpers/custom-validators';
+import { AuthService } from 'src/app/services/auth.service';
+import { LogInPayload } from 'src/app/interfaces/payload.interface';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.scss']
 })
-export class LogInComponent {
+export class LogInComponent implements OnInit {
   loginForm!: FormGroup;
+  logInEmail!: string;
+  logInPassword!: string;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.initializeForm();
@@ -18,13 +22,8 @@ export class LogInComponent {
 
   initializeForm(): void {
     this.loginForm = new FormGroup({
-      email: new FormControl(null, [
-        Validators.required,
-        Validators.email]),
-      password: new FormControl(null, [
-        Validators.required,
-        CustomValidators.passwordValidator,
-      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, CustomValidators.passwordValidator]),
       remember: new FormControl(false),
     });
   }
@@ -39,6 +38,22 @@ export class LogInComponent {
 
   get remember(): FormControl {
     return this.loginForm.get('remember') as FormControl;
+  }
+
+  logIn() {
+    var payload: LogInPayload = {
+      email: this.logInEmail,
+      password: this.logInPassword
+    }
+    // TO BE CHANGED LATER
+    if(this.loginForm.valid && this.authService.successLogIn(payload)) {
+      alert("VALID FORM");
+      return;
+    }
+    else{
+      alert("INVALID FORM");
+      return;
+    }
   }
 
   // logIn() {
