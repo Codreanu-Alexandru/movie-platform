ce2f7f9356d4441a1d4e7e1d0b1d5cae93cd8585
Radu-Branzei <115561060+Radu-Branzei@users.noreply.github.com>
2023-06-29 17:24:31 +0300

    Modified routes. (NEEDS TO BE CHANGED)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './log-in-module/log-in/log-in.component';
+import { AuthGuard } from './guards/auth.guard';
+import { RegisterFormComponent } from './register-module/register-form/register-form.component';
+
 
 const routes: Routes = [
+  //TO BE MODIFIED
   {
     path: 'login',
     loadChildren: () => import('./log-in-module/log-in.module').then(m => m.LogInModule)
   },
   {
-    path: '',
-    loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule)
+    path: 'regis',
+    loadChildren: () => import('./register-module/register.module').then(m => m.RegisterModule)
+  },
+  {
+    path: 'register', component: RegisterFormComponent
+  },
+  {
+    path: '', component: LogInComponent
+  },
+  {
+    path: 'main-table',
+    loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule),
+    canActivate: [AuthGuard]
   }
 ];
 
